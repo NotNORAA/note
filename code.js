b5e4bd30f40e0d4d6fdc4ad0ea2b7d0c6e3719bf
NotNORAA <sarcenoaaron94@gmail.com>
2024-09-07 18:38:03 +0800

    Update code.js

--- code.js
+++ code.js
@@ -12,7 +12,7 @@ function updateStorage() {
 }
 
 createBtn.addEventListener("click", ()=>{
-    let inputBox = document.createElement("p");
+    let inputBox = document.createElement("P");
     let img = document.createElement("img");
     inputBox.className = "input-box";
     inputBox.setAttribute("contenteditable", "true");
@@ -25,7 +25,7 @@ notesContainer.addEventListener("click", function(e){
         e.target.parentElement.remove();
         updateStorage();
     }
-    else if(e.target.tagName === "p"){
+    else if(e.target.tagName === "P"){
         notes = document.querySelectorAll(".input-box");
         notes.forEach(nt => {
             nt.onkeyup = function(){
