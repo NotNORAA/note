ee9877e17c6c3983ce6ad0fe68dd57451caad883
NotNORAA <sarcenoaaron94@gmail.com>
2024-09-07 18:40:17 +0800

    Update code.js

--- code.js
+++ code.js
@@ -12,7 +12,7 @@ function updateStorage() {
 }
 
 createBtn.addEventListener("click", ()=>{
-    let inputBox = document.createElement("P");
+    let inputBox = document.createElement("p");
     let img = document.createElement("img");
     inputBox.className = "input-box";
     inputBox.setAttribute("contenteditable", "true");
